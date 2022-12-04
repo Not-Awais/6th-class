0d228cfce4789d1c3738bd3235413a719d2194b7
Awais Mughal <114757869+Not-Awais@users.noreply.github.com>
2022-12-04 20:57:14 +0500

    commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// var arryOfStudents = ["awais", "faiz", "hafeez", "farhan", "huzaifa", "qadeer"];
+
+// var student = "awais"
+
+
+// let str = "Hello India";
+
+// let split_str = str.split("");
+// console.log(split_str);
+
+// console.log(arryOfStudents);
+// arryOfStudents.reverse()
+
+// student.split("a")
+
+// console.log(student);
+
+// console.log(arryOfStudents.length);
+
+// arryOfStudents.pop();
+// arryOfStudents.push("lal fua")
+
+// arryOfStudents.shift();
+// arryOfStudents.unshift("lal fua")
+
+// arryOfStudents.splice(2, 1, "lal fua");
+
+// var response = arryOfStudents.slice(0, 4)
+
+// var response = arryOfStudents.pop()
+// var response = arryOfStudents.push("lal fua")
+
+// var response = arryOfStudents.shift()
+// var response = arryOfStudents.unshift()
+
+// var response = arryOfStudents.splice(0, 1)
+// console.log(response);
+
+// console.log(arryOfStudents);
+
+
+// var nam = "awais";
+
+// var split = nam.split("");
+// console.log(split);
+
+// var join = split.join("")
+
+// console.log(join);
+
+
+
